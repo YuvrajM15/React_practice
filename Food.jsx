9d6cb53928f028eeb888bbd25bc38dafe1687da2
YuvrajM15 <mryuvraj15@gmail.com>
2025-06-10 00:55:14 +0530

    Add files via upload

---
+++ Food.jsx
@@ -0,0 +1,14 @@
+
+function Food(){
+    const food1="Apple";
+    const food2="Banana";
+    const food3="watermellon";
+
+    return (
+        <ul>
+            <li>Apple</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+export default Food
